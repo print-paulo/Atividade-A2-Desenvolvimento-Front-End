a99800da437902b2c3043de8026bd38ea8f25f9a
Paulo Henrique <paulohenriquenscmnt06@gmail.com>
2025-10-28 08:14:24 -0300

    Ajustado posição do desenho dentro do canva

--- js/script.js
+++ js/script.js
@@ -8,15 +8,15 @@ function desenharFatiaDePizza() {
     const ctx = canvas.getContext('2d');
 
     // Centraliza o centro da pizza
-    const centerX = 20; 
-    const centerY = 40; 
+    const centerX = 49; 
+    const centerY = 42; 
     const raio = 30; 
 
     // Desenhando a fatia da pizza
     ctx.beginPath(); 
     ctx.moveTo(centerX, centerY); 
 
-    const anguloInicial = 280 * (Math.PI / 180); // Convertendo graus para radianos
+    const anguloInicial = 200 * (Math.PI / 180); // Convertendo graus para radianos
 
     // Desenha o arco da fatia (o lado curvo)
     ctx.arc(
@@ -38,14 +38,7 @@ function desenharFatiaDePizza() {
     ctx.stroke();
 
     // Adiciona um topping (círculo vermelho, como um pepperoni)
-    ctx.beginPath();
     
-    // Posiciona o topping na fatia
-    ctx.arc(centerX + 10, centerY -10, 3, 0, Math.PI * 2); 
-    ctx.fillStyle = '#bd3623'; 
-    ctx.fill();
-    
-    ctx.closePath(); // Fecha o caminho principal
 }
 
 window.onload = desenharFatiaDePizza;
