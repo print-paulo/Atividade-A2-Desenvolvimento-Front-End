7013881cdeafa07f93d24cbed5beba836c1212a4
Paulo Henrique <paulohenriquenscmnt06@gmail.com>
2025-10-28 08:37:23 -0300

    Feito toppings no icone da pizza

--- js/script.js
+++ js/script.js
@@ -9,7 +9,7 @@ function desenharFatiaDePizza() {
 
     // Centraliza o centro da pizza
     const centerX = 49; 
-    const centerY = 42; 
+    const centerY = 43; 
     const raio = 30; 
 
     // Desenhando a fatia da pizza
@@ -36,9 +36,28 @@ function desenharFatiaDePizza() {
     ctx.strokeStyle = '#d6ad55ff'; 
     ctx.lineWidth = 2; // Borda ajustada para 2px para o ícone
     ctx.stroke();
-
-    // Adiciona um topping (círculo vermelho, como um pepperoni)
     
+    const posicoesTopping = [
+        {x: 38, y:23},
+        {x: 40, y:33},
+        {x: 30, y:30}
+    ];
+
+    for (const pos of posicoesTopping) {
+        ctx.beginPath();
+
+        ctx.arc(
+            pos.x,
+            pos.y,
+            2.7,
+            0,
+            Math.PI * 2
+        );
+
+        ctx.fillStyle = "#bd3623";
+        ctx.fill();
+        ctx.closePath();
+    }
 }
 
 window.onload = desenharFatiaDePizza;
